b8b7ddd2233ee8005432590079041716a81dafa6
Yaroslav Symonenko <y@madappgang.com>
2020-10-11 21:07:33 +0300

    Add tests for MAGBackendLogSender

--- Tests/MAGLoggerTests/MAGBackendLogSenderTests.swift
+++ Tests/MAGLoggerTests/MAGBackendLogSenderTests.swift
@@ -1,8 +1,23 @@
-//
-//  File.swift
-//  
-//
-//  Created by Yaroslav Symonenko on 11.10.2020.
-//
+import XCTest
+@testable import MAGLogger
 
-import Foundation
+final class MAGBackendLogSenderTests: XCTestCase {
+    
+    func testSendToInvalidServerAsync() {
+        // invalid address
+        let serverURL = URL(string: "https://foo.bar.com")!
+        let logSender = MAGBackendLogSender(serverURL: serverURL)
+        
+        let exp = expectation(description: "returns false due to invalid URL")
+
+        logSender.sendToServerAsync([:]) { ok in
+            XCTAssertFalse(ok)
+            exp.fulfill()
+        }
+        waitForExpectations(timeout: 60, handler: nil)
+    }
+    
+    static var allTests = [
+        ("testSendToInvalidServerAsync", testSendToInvalidServerAsync),
+    ]
+}
